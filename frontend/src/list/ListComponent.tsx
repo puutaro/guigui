0ffe9c6044e47a226d694c83956e3f2c96de8c96
puutaro <hoperstep@gmail.com>
2026-09-07 00:27:10 +0900

    input bug fix by timeout

--- frontend/src/list/ListComponent.tsx
+++ frontend/src/list/ListComponent.tsx
@@ -82,7 +82,7 @@ export const  ListComponent =
                     }
                     // フォーカスは常に input 要素へ
                     searchInputRef.current?.focus();
-                }, 200)
+                }, 300)
             });
             // タイマーとrAFの両方をクリーンアップ
             return () => {
